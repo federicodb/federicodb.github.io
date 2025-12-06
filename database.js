17aedb7a5dbea299b2d1fd443a2f9682b9b57305
federicodb <vivasantarosa@gmail.com>
2025-12-06 12:34:39 +0100

    correzione ort

--- database.js
+++ database.js
@@ -8,21 +8,21 @@ const db = [
         title: "Classificazione di Funzioni",
         date: "2025-12-6", // Formato YYYY-MM-DD per ordinamento
         url: "lezioni/math_underground003_4el.html",
-        tags: ["Funzioni", "DOminio", "Classificazione", "Onde", "Seno", "Laboratorio"],
+        tags: ["Matematica", "Funzioni", "DOminio", "Classificazione", "Onde", "Seno", "Laboratorio"],
         excerpt: "Mappa per la classificazione di funzioni"
     },
     {
         title: "Scomposizione di polinomi",
         date: "2025-11-15",
         url: "lezioni/cards_polinomi_memory004.html",
-        tags: ["Matematica", "Polinomi", "Scomposizio", "Algebra", "Memo"],
+        tags: ["Matematica", "Polinomi", "Scomposizioni", "Algebra", "Memo"],
         excerpt: "Visualizzare graficamente le principali scomposizioni di polinomi"
     },
     {
         title: "Operazioni in Z",
         date: "2025-10-20",
         url: "lezioni/memory_1el_numeri_004.html",
-        tags: ["Matematica", "Potenze", "Operazioni", "Proprietà", "Errori"],
+        tags: ["Matematica", "Potenze", "Operazioni", "Algebra", "Proprietà", "Errori"],
         excerpt: "Impariamo a lavorare con i numeri interi"
     },
     // Aggiungi qui le nuove lezioni...
